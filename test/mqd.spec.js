ce018143a0caae2d30b9f4151b2030258d15b329
Kirill Lahtachev <lahtachev@gmail.com>
2018-07-02 17:48:46 +0400

    new tests added

--- test/mqd.spec.js
+++ test/mqd.spec.js
@@ -1,3 +1,4 @@
+const { join, resolve } = require('path');
 const mqd = require('../lib/mqd');
 
 describe('test mqd functions', () => {
@@ -45,6 +46,27 @@ describe('test mqd functions', () => {
         assert.isOk('loading commands', 'command exists');
       } catch (error) {
         assert.isNotOk('loading commands', 'error was caught');
+      }
+    });
+  });
+
+  describe('load', () => {
+    it('detect files without extension .js', async () => {
+      try {
+        mqd.commandsPath = resolve(join(__dirname, '..', 'bin'));
+        expect(await mqd.load()).to.be.true;
+      } catch (error) {
+        assert.isNotOk('loading', 'error was caught');
+      }
+    });
+
+    it('catch error in loading', async () => {
+      try {
+        mqd.commandsPath = './non-existent-directory';
+        await mqd.load();
+        assert.isNotOk('readdir', `error wasn't caught`);
+      } catch (error) {
+        assert.isOk('readdir', 'error was caught');
       }
     });
   });
