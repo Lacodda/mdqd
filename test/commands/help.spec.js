e0c8820e038ca1cab2eeb4bdb80a0d98c141f9d2
Kirill Lahtachev <lahtachev@gmail.com>
2018-07-28 22:27:12 +0400

    help command updated

--- test/commands/help.spec.js
+++ test/commands/help.spec.js
@@ -1,14 +1,12 @@
 const exec = require('../../lib/util/exec');
-// const help = require('../../lib/help');
 
 describe('test help functions', () => {
   describe('cli', () => {
     it('prints a general help message', async () => {
-      const result = await exec(`${bin}`);
+      const result = await exec(`${bin} help`);
 
-      assert.ok(/Usage: mqd/.test(result));
-      assert.ok(/help/.test(result));
-      assert.ok(/init/.test(result));
+      assert.ok(/mqd convert/.test(result));
+      assert.ok(/mqd init/.test(result));
     });
   });
 });
